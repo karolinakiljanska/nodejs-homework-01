019c6af3cd0b2d19c128bef167dfb661c660527c
karolinakiljanska <karolinakiljanska112@gmail.com>
2023-10-20 14:25:04 +0200

    usunięcie komtarzy

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
       console.log(removeById);
       break;
 
-    // default:
-    //   console.warn("\x1B[31m Unknown action type!");
+    default:
+      console.warn("\x1B[31m Unknown action type!");
   }
 };
 
